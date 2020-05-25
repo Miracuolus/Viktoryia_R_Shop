449b6bf33d3bd24cd3bb5ea16e1779d1e081944f
Vika Ratomskaya <ratom_v@mail.ru>
2020-05-25 22:18:54 +0300

    Added format Hello World!

--- src/shopapp/views.py
+++ src/shopapp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def test(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return HttpResponse('<h1 align = "center"><i>Hello World!</i></h1>')
