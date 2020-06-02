08541e25cc58a6f02f3a5e4ca93909e1bf74b3b9
Vika Ratomskaya <ratom_v@mail.ru>
2020-06-02 11:53:53 +0300

    Changed BD, changed file models

--- 05_06_Django_Parser_Apache/src/parser_apache/models.py
+++ 05_06_Django_Parser_Apache/src/parser_apache/models.py
@@ -1,37 +1,19 @@
 from django.db import models
-from Parser_Apache_logs import Ratomskaya_parser_apache_logs as Parser
+
 
 # Create your models here.
-
-class Row_line(models.Model):
-    name = models.CharField(
-        verbose_name='Строка лога',
-        max_length=400
+class Parser(models.Model):
+    log = models.TextField(
+        verbose_name='Лог',
     )
-
-    def __str__(self):
-        return self.name
-
-class Time_from_log(models.Model):
-    name = models.DateTimeField(
+    time = models.DateTimeField(
         verbose_name='Дата и время записи',
     )
-    line = models.ForeignKey(
-        Row_line,
-        on_delete=models.PROTECT,
-        verbose_name='Соответствующая дата',
-    )
-    def __str__(self):
-        return self.name
-
-class Brousers(models.Model):
-    brousers = models.ForeignKey(
-        Row_line,
-        on_delete=models.PROTECT,
-        verbose_name='Браузеры',
-    )
-    name = models.TextField(
+    brouser = models.TextField(
         verbose_name='Браузеры',
         null=True,
         blank=True
     )
+
+    def __str__(self):
+        return self.time
--- 05_06_Django_Parser_Apache/src/parser_apache/views.py
+++ 05_06_Django_Parser_Apache/src/parser_apache/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from Parser_Apache_logs import Ratomskaya_parser_apache_logs as Parser
+from . models import Parser
+
+
 
 # Create your views here.
 
 def parser_apache(request):
-    context = {}
+    with open('apache_logs/apache_logs_small.txt', 'r') as fp:
+        count = 0
+        for line in fp.readlines():
+            count += 1
+            #l = Row_line.objects.create(pk=count,name=line)
+            #l.save()
+    context = {'count':count, 'line':line}
+    
     return render(request, template_name='parser_apache/index.html', context=context)
+
+
