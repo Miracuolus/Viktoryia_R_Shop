d4bfad9fabab27f8ca6a5caf6e22fcf89aa14758
Vika Ratomskaya <ratom_v@mail.ru>
2020-06-05 23:30:06 +0300

    Changed a few changng in DB

--- 08_06_Django_Genre/src/bookapp/models.py
+++ 08_06_Django_Genre/src/bookapp/models.py
@@ -3,13 +3,14 @@ from django.db import models
 # Create your models here.
 class Genre(models.Model):
     name = models.CharField(
-        verbose_name='Название жанра', # в читаемом виде
+        verbose_name='Название жанра',
         max_length= 100
     )
     description = models.TextField(
         verbose_name='Описание жанра',
         null=True, # БД позволяет хранить поле пустым
-        blank=True # сигнал Djange, что поле мб пустым
+        blank=True, # сигнал Djange, что поле мб пустым
+        help_text='Поле может быть пустым'
     )
 
     def __str__(self):
@@ -18,30 +19,29 @@ class Genre(models.Model):
 
 class Book(models.Model):
     name = models.CharField(
-        verbose_name='Название книги', # в читаемом виде
+        verbose_name='Название книги',
         max_length= 200
     )
     genre = models.ForeignKey(
         Genre,
-        on_delete=models.PROTECT, # защищать от удаления
+        on_delete=models.PROTECT,
         verbose_name='Жанр книги',
     )
     description = models.TextField(
         verbose_name='Описание книги',
         null=True, # БД позволяет хранить поле пустым
-        blank=True # сигнал Djange, что поле мб пустым
+        blank=True, # сигнал Djange, что поле мб пустым
+        help_text='Поле может быть пустым'
     )
     created = models.DateTimeField(
         verbose_name='Создано',
         auto_now=False, # автом ставить тек время
-        auto_now_add=True # автом ставить время добавления
+        auto_now_add=True, # автом ставить время добавления
     )
     updated = models.DateTimeField(
         verbose_name='Изменено',
         auto_now=True, # автом ставить тек время
         auto_now_add=False # автом ставить время добавления
     )
-
-
     def __str__(self):
-        return self.name
+        return f'{self.name}: жанр - {self.genre}'
