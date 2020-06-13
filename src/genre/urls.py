31a73afe8a48cfa45e40d6c878da2c3fe65296b2
Vika Ratomskaya <ratom_v@mail.ru>
2020-06-13 22:32:00 +0300

    Changed all links in according with name urls

--- src/genre/urls.py
+++ src/genre/urls.py
@@ -3,9 +3,10 @@ from  . views import CreateGenre, UpdateGenre, GenreList, DeleteGenre, DetailGen
 app_name = 'genre'
 
 urlpatterns = [
-    path('create/', CreateGenre.as_view(), name='create_genre'), # добавление элементов в таблицу Жанры
-    path('update/<int:pk>', UpdateGenre.as_view(), name='update_genre'), # обновление таблицы Жанры
-    path('deletegenre/<int:pk>', DeleteGenre.as_view(), name='delete_genre'), # удаление жанров
-    path('list/', GenreList.as_view(), name='list_genre'), # список жанров
-    path('detail/<int:pk>', DetailGenre.as_view(), name='detail_genre'), # справочник жанра
+    path('create/', CreateGenre.as_view(), name='create'), # добавление элементов в таблицу Жанры
+    path('update/<int:pk>', UpdateGenre.as_view(), name='update'), # обновление таблицы Жанры
+    path('deletegenre/<int:pk>', DeleteGenre.as_view(), name='delete'), # удаление жанров
+    path('list/', GenreList.as_view(), name='list'), # список жанров
+    path('detail/<int:pk>', DetailGenre.as_view(), name='detail'), # справочник жанра
+    path('', GenreList.as_view()), # список жанров
 ]
--- src/genre/views.py
+++ src/genre/views.py
@@ -8,38 +8,45 @@ from django.views.generic import (
                                     DeleteView,
                                     DetailView
                                 )
+from django.urls import reverse_lazy
 
 # Create your views here.
 class CreateGenre(CreateView):
     model = Genre
     form_class = GenreForm
     template_name = 'genre/create_genre.html'
-    success_url = '/main/'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['creategenre'] = GenreForm()
         return context
+    
+    def get_success_url(self):
+        return reverse_lazy('genre:list')
 
 
 class UpdateGenre(UpdateView):
     model = Genre
     form_class = GenreForm
     template_name = 'genre/update_genre.html'
-    #success_url = '/update/'
-    success_url = '/main/'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['updategenre'] = GenreForm(instance=Genre.objects.get(pk=self.kwargs['pk']))
         #UpdateGenre.success_url = '/update/' + str(self.kwargs['pk'])
         return context
+    
+    def get_success_url(self):
+        return reverse_lazy('genre:list')
 
 class DeleteGenre(DeleteView):
     model = Genre
     template_name = 'genre/delete_genre.html'
     context_object_name = 'genre'
-    success_url = '/main/'
+
+    def get_success_url(self):
+        return reverse_lazy('genre:list')
+
 
 class GenreList(ListView):
     template_name = 'genre/list_genre.html'
@@ -50,3 +57,6 @@ class GenreList(ListView):
 class DetailGenre(DetailView):
     model = Genre
     template_name = 'genre/detail_genre.html'
+
+    def get_success_url(self):
+        return reverse_lazy('genre:detail', kwargs={'pk':self.object.pk})
