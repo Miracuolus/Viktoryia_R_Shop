da5775bf2bb72d08bfeb404962193e2d1d3667ce
Vika Ratomskaya <ratom_v@mail.ru>
2020-06-13 23:52:32 +0300

    FIX

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -58,9 +58,8 @@ ROOT_URLCONF = 'proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR), 'templates'],
-        'APP_DIRS': False,
-        #'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
