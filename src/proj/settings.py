23d35c6cf944369b318d4d2f4c80c26b702aa107
Vika Ratomskaya <ratom_v@mail.ru>
2020-06-13 23:39:23 +0300

    'APP_DIRS': False

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -59,7 +59,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR), 'templates'],
-        'APP_DIRS': True,
+        'APP_DIRS': False,
+        #'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
