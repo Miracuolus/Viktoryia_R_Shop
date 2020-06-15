f60e62f2521096caa58262410dcd8c8896bf4510
Vika Ratomskaya <ratom_v@mail.ru>
2020-06-15 13:57:32 +0300

    Added app series

---
+++ src/series/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Series(models.Model):
+    name = models.CharField(
+        verbose_name='Серия',
+        max_length= 200
+    )
+
+    description = models.TextField(
+        verbose_name='Описание',
+        null=True,
+        blank=True,
+        help_text='Поле может быть пустым'
+    )
+
+    class Meta: 
+        ordering = ['name']
+    
+    def __str__(self):
+        return f'{self.name}'
