eb005b53bb01510f3bb09ac891dc96bd9c589828
Vika Ratomskaya <ratom_v@mail.ru>
2020-06-28 17:34:05 +0300

    Added function for Cart

--- src/cart/views.py
+++ src/cart/views.py
@@ -1,14 +1,40 @@
-from . models import Book
-from . forms import BookForm
-from django.views.generic import (
-                                    TemplateView, 
-                                    CreateView, 
-                                    UpdateView, 
-                                    ListView, 
-                                    DeleteView,
-                                    DetailView
-                                )
+from . models import Cart
+from . forms import CartForm
+from django.views.generic import (TemplateView, 
+                                  CreateView, 
+                                  UpdateView, 
+                                  ListView, 
+                                  DeleteView,
+                                  DetailView
+)
 from django.urls import reverse_lazy
 import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin # залогиненные пользователи
 from django.core.paginator import Paginator
+
+# Create your views here.
+class CreateCart(LoginRequiredMixin, CreateView):
+    model = Cart
+    form_class = CartForm
+     
+    def get_success_url(self):
+        return reverse_lazy('cart:detail', kwargs={'pk':self.object.pk})
+
+class UpdateCart(LoginRequiredMixin, UpdateView):
+    model = Cart
+    form_class = CartForm
+    
+    def get_success_url(self):
+        return reverse_lazy('cart:detail', kwargs={'pk':self.object.pk})
+
+class DeleteCart(LoginRequiredMixin, DeleteView):
+    model = Cart
+
+    def get_success_url(self):
+        return reverse_lazy('cart:detail', kwargs={'pk':self.object.pk})
+
+class DetailCart(DetailView):
+    model = Cart
+
+    def get_success_url(self):
+        return reverse_lazy('book:detail', kwargs={'pk':self.object.pk})
