d378f66deb8f589ccdb8a42dc7797104440dca34
Vika Ratomskaya <ratom_v@mail.ru>
2020-06-30 21:14:47 +0300

    Print all price for book in cart

--- src/proj/templatetags/app_tags.py
+++ src/proj/templatetags/app_tags.py
@@ -3,6 +3,7 @@ from django import template
 register = template.Library()
 
 from appinfo.models import AppInfo
+from decimal import Decimal
 
 
 
@@ -20,6 +21,17 @@ def print_app_year():
     info = AppInfo.objects.get(pk=1)
     return info.year
 
+sum_price = 0
 @register.simple_tag
 def multiply(value1, value2):
+    global sum_price
+    sum_price += value1*value2
     return value1 * value2
+
+
+@register.simple_tag
+def sum_filter():
+    global sum_price
+    price = sum_price
+    sum_price = 0
+    return price
