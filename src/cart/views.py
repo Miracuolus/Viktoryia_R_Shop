c3ccbbc4d57b49a1ef693b0d4ff9c25221ee8932
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-03 17:46:02 +0300

    When clicl to add book to cart, the site open cart

--- src/cart/views.py
+++ src/cart/views.py
@@ -16,13 +16,6 @@ from decimal import Decimal
 from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
-class CreateCart(LoginRequiredMixin, CreateView):
-    model = BooktoCart
-    form_class = CartForm
-     
-    def get_success_url(self):
-        return reverse_lazy('cart:detail', kwargs={'pk':self.object.pk})
-
 class UpdateCart(LoginRequiredMixin, UpdateView):
     model = BooktoCart
     form_class = CartForm
@@ -70,8 +63,23 @@ class ListCart(ListView):
 
 class AddBooktoCart(UpdateView):
     model = BooktoCart
-    template_name = 'cart/add_cart.html'
-    fields = ('quantity',)
+    #template_name = 'cart/add_cart.html'
+    #fields = ('quantity',)
+    form_class = CartForm
+    template_name = 'cart/list_cart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_pk = self.request.session.get('cart_pk')
+        user = self.request.user
+        if user.is_authenticated:
+            cart = Cart.objects.filter(pk=cart_pk, user=user)
+        else:
+            cart = Cart.objects.filter(pk=cart_pk)
+        if cart:
+            book_in_cart = self.model.objects.all().filter(cart=cart[0])
+            context['object_list'] = book_in_cart
+            return context
 
     def get_success_url(self):
         return reverse_lazy('cart:list')
