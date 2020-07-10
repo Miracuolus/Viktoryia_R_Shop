2b000b6088ec61cf66c5b22723aebb018269aaf4
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-10 14:52:46 +0300

    Addedp permission for update/create/delete genre

--- src/genre/urls.py
+++ src/genre/urls.py
@@ -5,7 +5,7 @@ app_name = 'genre'
 urlpatterns = [
     path('create/', CreateGenre.as_view(), name='create'),
     path('update/<int:pk>', UpdateGenre.as_view(), name='update'),
-    path('deletegenre/<int:pk>', DeleteGenre.as_view(), name='delete'),
+    path('delete/<int:pk>', DeleteGenre.as_view(), name='delete'),
     path('list/', GenreList.as_view(), name='list'),
     path('detail/<int:pk>', DetailGenre.as_view(), name='detail'),
 ]
