3ce97810d4ac260703497f2b3d543a14af3a90df
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-10 15:27:51 +0300

    Added permissions for create/delete/update series

--- src/series/urls.py
+++ src/series/urls.py
@@ -6,7 +6,7 @@ app_name = 'series'
 urlpatterns = [
     path('create/', CreateSeries.as_view(), name='create'),
     path('update/<int:pk>', UpdateSeries.as_view(), name='update'),
-    path('deletegenre/<int:pk>', DeleteSeries.as_view(), name='delete'),
+    path('delete/<int:pk>', DeleteSeries.as_view(), name='delete'),
     path('list/', SeriesList.as_view(), name='list'),
     path('detail/<int:pk>', DetailSeries.as_view(), name='detail'),
 ]
