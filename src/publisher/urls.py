9bb0a6240e9c0d72bf145b31ee0dd411075059f6
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-10 15:24:52 +0300

    Added permission for create/update/delete publishers

--- src/publisher/urls.py
+++ src/publisher/urls.py
@@ -5,7 +5,7 @@ app_name = 'publisher'
 urlpatterns = [
     path('create/', CreatePublisher.as_view(), name='create'),
     path('update/<int:pk>', UpdatePublisher.as_view(), name='update'),
-    path('deletegenre/<int:pk>', DeletePublisher.as_view(), name='delete'),
+    path('delete/<int:pk>', DeletePublisher.as_view(), name='delete'),
     path('list/', PublisherList.as_view(), name='list'),
     path('detail/<int:pk>', DetailPublisher.as_view(), name='detail'),
 ]
