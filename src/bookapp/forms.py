dc06d0efc50cc7ee87b63bfb5343c9ecc40004a7
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-11 23:20:46 +0300

    Created added to database genre and authors from csv file

--- src/bookapp/forms.py
+++ src/bookapp/forms.py
@@ -44,15 +44,46 @@ class ImportForm(forms.ModelForm):
     def process_file(self):
         data = io.TextIOWrapper(self.cleaned_data['file_books'].file)
         reader = csv.DictReader(data)
+        count_author = 0
+        author_set = set()
+        count_genre = 0
+        genre_set = set()
         for b in reader:
-            author, created = Author.objects.get_or_create(
-                    name = b['author1'],
-                    defaults = {}
-                    )
-            genre, created = Genre.objects.get_or_create(
-                    name = b['genre1'],
-                    defaults = {}
-                    )
+
+            if b['author1']:
+                if b['author2']:
+                    if b['author3']:
+                        count_author = 3
+                    else:
+                        count_author = 2
+                else:
+                    count_author = 1
+            author_list = (b['author1'], b['author2'], b['author3'])
+
+            for a in range(0, count_author):
+                author, created = Author.objects.get_or_create(
+                        name = author_list[a],
+                        defaults = {}
+                        )
+                author_set.add(author)
+
+            if b['genre1']:
+                if b['genre2']:
+                    if b['genre3']:
+                        count_genre = 3
+                    else:
+                        count_genre = 2
+                else:
+                    count_genre = 1
+            genre_list = (b['genre1'], b['genre2'], b['genre3'])
+
+            for g in range(0, count_genre):
+                genre, created = Genre.objects.get_or_create(
+                        name = genre_list[g],
+                        defaults = {}
+                        )
+                genre_set.add(genre)
+
             series, created = Series.objects.get_or_create(
                     name = b['series'],
                     defaults = {}
@@ -65,7 +96,6 @@ class ImportForm(forms.ModelForm):
                 name = b['name'],
                 photo = b['photo'],
                 price = float(b['price']),
-                #author  = author,
                 series  = series,
                 #genre  = genre,
                 year  = b['year'],
@@ -82,6 +112,9 @@ class ImportForm(forms.ModelForm):
                 defaults = {}
             )
             #Book.objects.filter(name=b['name']).update(quantity = book.quantity + int(b['quantity']))
+            B = Book.objects.filter(name=b['name']).first()
+            B.author.add(*author_set)
+            B.genre.add(*genre_set)
         return data
         
     class Meta:
