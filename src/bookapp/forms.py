514b76abe0f6cebb8d05c92167af4843dc475762
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-12 21:13:00 +0300

    Added full fields for import

--- src/bookapp/forms.py
+++ src/bookapp/forms.py
@@ -44,42 +44,21 @@ class ImportForm(forms.ModelForm):
     def process_file(self):
         data = io.TextIOWrapper(self.cleaned_data['file_books'].file)
         reader = csv.DictReader(data)
-        count_author = 0
         author_set = set()
-        count_genre = 0
         genre_set = set()
         for b in reader:
-
-            if b['author1']:
-                if b['author2']:
-                    if b['author3']:
-                        count_author = 3
-                    else:
-                        count_author = 2
-                else:
-                    count_author = 1
-            author_list = (b['author1'], b['author2'], b['author3'])
-
-            for a in range(0, count_author):
+            authors = b['author'].split(', ')
+            genres = b['genre'].split(', ')
+            for a in range(0, len(authors)):
                 author, created = Author.objects.get_or_create(
-                        name = author_list[a],
+                        name = authors[a],
                         defaults = {}
                         )
                 author_set.add(author)
 
-            if b['genre1']:
-                if b['genre2']:
-                    if b['genre3']:
-                        count_genre = 3
-                    else:
-                        count_genre = 2
-                else:
-                    count_genre = 1
-            genre_list = (b['genre1'], b['genre2'], b['genre3'])
-
-            for g in range(0, count_genre):
+            for g in range(0, len(genres)):
                 genre, created = Genre.objects.get_or_create(
-                        name = genre_list[g],
+                        name = genres[g],
                         defaults = {}
                         )
                 genre_set.add(genre)
@@ -97,7 +76,6 @@ class ImportForm(forms.ModelForm):
                 photo = b['photo'],
                 price = float(b['price']),
                 series  = series,
-                #genre  = genre,
                 year  = b['year'],
                 pages  = b['pages'],
                 bookbinding  = b['bookbinding'],
