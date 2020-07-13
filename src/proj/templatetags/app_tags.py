ca12df06a15ab99e8051af1a0fd566e579e0ab2a
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-13 22:36:16 +0300

    Deleted two tags because in model Cart added methods price

--- src/proj/templatetags/app_tags.py
+++ src/proj/templatetags/app_tags.py
@@ -24,24 +24,6 @@ def print_app_year():
     info = AppInfo.objects.get(pk=1)
     return info.year
 
-sum_price = 0
-sum_price_order = 0
-@register.simple_tag
-def multiply(value1, value2):
-    global sum_price
-    sum_price += value1*value2
-    return value1 * value2
-
-
-@register.simple_tag
-def sum_filter():
-    global sum_price
-    global sum_price_order
-    sum_price_order = sum_price
-    price = sum_price
-    sum_price = 0
-    return price
-
 @register.simple_tag
 def sum_order():
     global sum_price_order
