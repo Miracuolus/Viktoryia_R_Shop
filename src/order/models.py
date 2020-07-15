6e231d98f607e1214333bab37241474e451c76dd
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-15 14:00:59 +0300

    Created model Comment_Order

--- src/order/models.py
+++ src/order/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from cart.models import Cart
 from customers.models import Customer
 from decimal import Decimal
+from django.contrib.auth.models import Group
 
 # Create your models here.
 User = get_user_model()
@@ -86,3 +87,47 @@ class Order(models.Model):
     def __str__(self):
         return f'{self.user} {self.created}, status {self.status}'
 
+
+class Comment_Order(models.Model):
+    order = models.ForeignKey(
+        'Order',
+        on_delete=models.PROTECT,
+        verbose_name='Заказ',
+    )
+    comment = models.TextField(
+        verbose_name='Комментарий',
+        max_length= 100,
+        null=True,
+        blank=True,
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        verbose_name='Пользователь',
+    )
+    role_user = models.ForeignKey(
+        Group,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        verbose_name='Группа',
+    )
+    created = models.DateTimeField(
+        verbose_name='Создано',
+        auto_now=False, # автом ставить тек время
+        auto_now_add=True, # автом ставить время добавления
+    )
+    updated = models.DateTimeField(
+        verbose_name='Изменено',
+        auto_now=True, # автом ставить тек время
+        auto_now_add=False # автом ставить время добавления
+    )
+
+    class Meta: 
+        verbose_name = 'Комментарий к заказу'
+        verbose_name_plural = 'Комментарии к заказам'
+    
+    def __str__(self):
+        return f'{self.order}: {self.user} - {self.role_user }'
