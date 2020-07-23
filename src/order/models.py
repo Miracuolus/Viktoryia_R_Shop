5c48334cc848975c9ccfc746570b270a8f9e9c8e
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-23 18:09:01 +0300

    Change ordering for db Order

--- src/order/models.py
+++ src/order/models.py
@@ -86,7 +86,7 @@ class Order(models.Model):
         related_name='comments',
     )
     class Meta: 
-        ordering = ['status']
+        #ordering = ['status']
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
     
