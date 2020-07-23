1dfb240f322e45ccd5d7722278881a102296d931
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-23 15:05:38 +0300

    Created admin db customers

--- src/customers/admin.py
+++ src/customers/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from . models import Customer
 # Register your models here.
-admin.site.register(Customer)
+
+class Customer_Admin(admin.ModelAdmin):
+    list_display = ('user', 'group')
+admin.site.register(Customer, Customer_Admin)
