5dad3a1c505744f2291f0ae41f0f4eb4a6b17ce2
Vika Ratomskaya <ratom_v@mail.ru>
2020-07-23 15:03:45 +0300

    Created admin for db booktocart

--- src/cart/admin.py
+++ src/cart/admin.py
@@ -4,9 +4,11 @@ from . models import Cart, BooktoCart
 
 # Register your models here.
 
-admin.site.register(BooktoCart)
-
-
 class Cart_Admin(admin.ModelAdmin):
     list_display = ('pk', 'user', 'active')
 admin.site.register(Cart, Cart_Admin)
+
+
+class BooktoCart_Admin(admin.ModelAdmin):
+    list_display = ('cart', 'book', 'quantity')
+admin.site.register(BooktoCart, BooktoCart_Admin)
