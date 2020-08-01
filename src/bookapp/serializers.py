56f300bc3fa961cc237d32c84ea9c3ba27a37f6e
Vika Ratomskaya <ratom_v@mail.ru>
2020-08-01 14:03:16 +0300

    Print json with fields many to many

--- src/bookapp/serializers.py
+++ src/bookapp/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from . models import Book
+from author.serializers import AuthorSerializer
 from genre.serializers import GenreSerializer
+from publisher.serializers import PublisherSerializer
 from series.serializers import SeriesSerializer
 
 
 class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(many=True)
     series = SeriesSerializer()
-    #genre = GenreSerializer()
+    genre = GenreSerializer(many=True)
+    publisher = PublisherSerializer()
     class Meta:
         model = Book
         fields = ('pk',
