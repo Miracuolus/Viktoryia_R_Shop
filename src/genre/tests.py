9ec35ebfd2867f88637740a46e369120703dbd58
Vika Ratomskaya <ratom_v@mail.ru>
2020-08-05 23:04:35 +0300

    Added django test for genre

--- src/genre/tests.py
+++ src/genre/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+import unittest
+from .models import Genre
+#from unittest import mock
 
 # Create your tests here.
+class TestBook(unittest.TestCase):
+    def test_create_obj_has_pub_data(self):
+        post = Genre.objects.create(name='Name3')
+        self.assertIsNotNone(post.name)
