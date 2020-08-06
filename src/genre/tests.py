e9738f352ad95196212ea64bfbcae6622b1c5202
Vika Ratomskaya <ratom_v@mail.ru>
2020-08-06 22:55:03 +0300

    Added Django test for not null genre

--- src/genre/tests.py
+++ src/genre/tests.py
@@ -5,6 +5,9 @@ from .models import Genre
 
 # Create your tests here.
 class TestBook(unittest.TestCase):
+    def setUp(self):
+        Genre.objects.create(name='lion')
+
     def test_create_obj_has_pub_data(self):
-        post = Genre.objects.create(name='Name3')
+        post = Genre.objects.get(name='lion')
         self.assertIsNotNone(post.name)
